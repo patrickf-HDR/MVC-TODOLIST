17a91fcd6a7be3c818f809ed1e0130ccc269cdc8
Patrick Fiorentino <patrick.fiorentino@hdrinc.com>
2023-01-31 15:42:32 -0800

    Add project files.

---
+++ MvcToDo/Data/MvcToDoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcToDoList.Models;
+
+namespace MvcToDo.Data
+{
+    public class MvcToDoContext : DbContext
+    {
+        public MvcToDoContext (DbContextOptions<MvcToDoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcToDoList.Models.Task> Task { get; set; } = default!;
+    }
+}
---
+++ MvcToDo/Models/Task.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcToDoList.Models;
+
+public class Task
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime DueDate { get; set; }
+    public string? Category { get; set; }
+    public int Priority { get; set; }
+}
